b1449874fb893adfb641a0e211eb3f6bac70eb22
Thiago <thiagosrdg@gmail.com>
2024-12-10 20:51:12 -0300

    teste commit

---
+++ test1.js
@@ -0,0 +1,22 @@
+
+let x = 999; 
+let y = 666;
+let soma = x + y;
+console.log(soma);
+
+let nome = "thiago"
+let sobrenome = "rodrigues"
+console.log("meu nome é " + nome +" "+ sobrenome)
+
+let numerostring = "999"
+let numerostringdois = "666"
+soma = numerostring + numerostringdois;
+console.log(soma)
+
+// desafio
+let a = "thiago"
+let b = "rodrigues"
+let K = a
+a = b
+b = K
+console.log("meu nome é " + b +" "+ a)
